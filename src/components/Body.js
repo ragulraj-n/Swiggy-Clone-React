1e276dcb35878700933fa2fa9bdc58d08e6199f3
ragulraj-n <ragulrajrealme@gmail.com>
2025-11-01 09:56:50 +0530

    feat: add shimmer UI for restaurant cards

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,53 @@
 import ResCard from "./ResCard";
-import restaurants from "../utils/resData"
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import Shimmer from "./Shimmer";
     
 
 const Body = ()=>{
-    const [listRes,setListRes] = useState(restaurants);
+
     
+    const [restaurant,setRestaurant] = useState([]);
+    const [listRes,setListRes] = useState(restaurant);
+    
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async ()=>{
+        const apiData = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01020&lng=76.97010&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const jsonData = await apiData.json();
+        const data = jsonData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setRestaurant(data);
+        setListRes(data);
+    }
+
+    if(restaurant.length===0){
+        console.log("Shimmer Trigged");
+        return  (
+        <Shimmer />
+        )
+    }
     return (
      <div>
         <div className="btn-bar">
             <button className="top-rated-btn" onClick={()=>{
-            let filteredList = restaurants.filter((res)=>{
-                return  res.info.avgRating > 4.5;
+            let filteredList = restaurant.filter((res)=>{
+                return  res.info.avgRating > 4.2;
             })
+                console.log("Top Rated Button Clicked");
                 setListRes(filteredList);
             }}>Top Rated Restaurants</button>
 
             <button className="reset-btn" onClick={()=>{
-                setListRes(restaurants);
+                setListRes(restaurant);
+                console.log("Reset Button Clicked");
             }}>Reset Filters</button>
         </div>
             
         <div className="res-container">
-            {   listRes.map(
+            {  
+             listRes.map(
                 (data)=>{
                     return <ResCard  key={data.info.id} resData={data}/>
                 }
---
+++ src/components/Shimmer.js
@@ -0,0 +1,74 @@
+
+const Shimmer = () =>{
+    console.log("Inside Shimmer");
+    return(
+        
+        <div className="shimmer-container">
+                <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                 <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                 <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                 <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+                   <div className="shimmer-card">
+                    <div className="shimmer-card-img"></div>
+                </div>
+        </div>
+    )
+}
+
+export default Shimmer;
